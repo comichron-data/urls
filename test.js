4a66425a5ef3aebfe0592d9fae8dcaf89670eb1f
Paul Salaets <psalaets@viimed.com>
2016-10-06 21:12:45 -0400

    ensure it handles strings for year and month

--- test.js
+++ test.js
@@ -21,13 +21,15 @@ test('by issue', function(t) {
 });
 
 test('source data with single digit month', function(t) {
-  t.plan(1);
+  t.plan(2);
 
   t.equal(urls.sourceData(2001, 9), 'https://comichron-data.github.io/api/source-data/2001-09.json');
+  t.equal(urls.sourceData('2001', '9'), 'https://comichron-data.github.io/api/source-data/2001-09.json');
 });
 
 test('source data with double digit month', function(t) {
-  t.plan(1);
+  t.plan(2);
 
   t.equal(urls.sourceData(2001, 11), 'https://comichron-data.github.io/api/source-data/2001-11.json');
+  t.equal(urls.sourceData('2001', '11'), 'https://comichron-data.github.io/api/source-data/2001-11.json');
 });
