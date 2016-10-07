e8aa611878d6e0cb4cdc94158c26e1d37e7fe990
Paul Salaets <psalaets@viimed.com>
2016-10-06 21:24:04 -0400

    don't pad month if already padded

--- test.js
+++ test.js
@@ -8,28 +8,34 @@ test('titles', function(t) {
   t.equal(urls.titles(), 'https://comichron-data.github.io/api/titles.json');
 });
 
-test('by month', function(t) {
+test('byMonth', function(t) {
   t.plan(1);
 
   t.equal(urls.byMonth('blah'), 'https://comichron-data.github.io/api/titles/blah/by-month.json');
 });
 
-test('by issue', function(t) {
+test('byIssue', function(t) {
   t.plan(1);
 
   t.equal(urls.byIssue('blah'), 'https://comichron-data.github.io/api/titles/blah/by-issue.json');
 });
 
-test('source data with single digit month', function(t) {
+test('sourceData with single digit month', function(t) {
   t.plan(2);
 
   t.equal(urls.sourceData(2001, 9), 'https://comichron-data.github.io/api/source-data/2001-09.json');
   t.equal(urls.sourceData('2001', '9'), 'https://comichron-data.github.io/api/source-data/2001-09.json');
 });
 
-test('source data with double digit month', function(t) {
+test('sourceData with double digit month', function(t) {
   t.plan(2);
 
   t.equal(urls.sourceData(2001, 11), 'https://comichron-data.github.io/api/source-data/2001-11.json');
   t.equal(urls.sourceData('2001', '11'), 'https://comichron-data.github.io/api/source-data/2001-11.json');
+});
+
+test('sourceData only pads month if needed', function(t) {
+  t.plan(1);
+
+  t.equal(urls.sourceData('2001', '09'), 'https://comichron-data.github.io/api/source-data/2001-09.json');
 });
